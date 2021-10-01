efb773e863b56c19aa3dc4f419d084fa0818df13
Sewvka <hoteroke@gmail.com>
2021-10-01 11:49:58 +0700

    #HeapSort * HeapSort.java func swap_int added, define if array is Null * TestExample.java DisplayName added for all tests, new test for array == null

--- Java/src/main/java/HeapSort.java
+++ Java/src/main/java/HeapSort.java
@@ -1,22 +1,21 @@
 package main.java;
 
 public class HeapSort {
-    static public void sort(int[] arr)
-    {
+    public static void sort(int[] arr) {
+        if (arr == null) {
+            return;
+        }
         int len = arr.length;
         for (int i = len / 2 - 1; i >= 0; i--)
             heapify(arr, len, i);
         for (int i = len - 1; i > 0; i--) {
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
+            swap_int(arr, 0, i);
             heapify(arr, i, 0);
         }
     }
 
 
-    static private void heapify(int[] array, int len, int i)
-    {
+    private static void heapify(int[] array, int len, int i) {
         int max = i;
         int l = 2 * i + 1;
         int r = 2 * i + 2;
@@ -28,11 +27,13 @@ public class HeapSort {
             max = r;
 
         if (max != i) {
-            int swap = array[i];
-            array[i] = array[max];
-            array[max] = swap;
-
+            swap_int(array, i, max);
             heapify(array, len, max);
         }
+    }
+    private static void swap_int(int[] arr, int ind1, int ind2) {
+        int temp = arr[ind1];
+        arr[ind1] = arr[ind2];
+        arr[ind2] = temp;
     }
 }
